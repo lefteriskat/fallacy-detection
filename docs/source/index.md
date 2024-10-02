57a8e33abc6fba017be1446fdcb549134478680f
lefteriskat <lefteriskatiforis@gmail.com>
2024-10-02 10:01:16 +0200

    Initial cookiecutter project structure

---
+++ docs/source/index.md
@@ -0,0 +1,4 @@
+## Documentation
+
+Documentation for fallacy_detection
+
---
+++ fallacy_detection/__init__.py
@@ -0,0 +1,2 @@
+from fallacy_detection.models.model import MyNeuralNet
+from fallacy_detection.predict_model import predict
